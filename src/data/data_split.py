dade81f5ddd612477ba2486e4f8bd2bb3edeb040
GregoryRoquin <roquin.gregory@gmail.com>
2025-11-05 15:56:13 +0000

    MAJ data_split

--- src/data/data_split.py
+++ src/data/data_split.py
@@ -1,19 +1,29 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import click
+import os
 
-def main():
-    file_path = "https://datascientest-mlops.s3.eu-west-1.amazonaws.com/mlops_dvc_fr/raw.csv"
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=False), required=False)
+@click.argument('output_path', type=click.Path(exists=False), required=False)
+def main(input_filepath, output_path):
+    if not input_filepath:
+        input_filepath = click.prompt('Enter the file path for the input data', type=click.Path(exists=True))
+    if not output_path:
+        output_path = click.prompt('Enter the path for the output preprocessed data', type=click.Path())
     
-    df = pd.read_csv(file_path, header=0)
+    df = pd.read_csv(input_filepath, header=0)
 
     y = df["silica_concentrate"]
     X = df.drop(["silica_concentrate", "date"], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-    X_train.to_csv("data/processed_data/X_train.csv", index=False)
-    X_test.to_csv("data/processed_data/X_test.csv", index=False)
-    y_train.to_csv("data/processed_data/y_train.csv", index=False)
-    y_test.to_csv("data/processed_data/y_test.csv", index=False)
+    os.makedirs(output_path, exist_ok=True)
+
+    X_train.to_csv(os.path.join(output_path,"X_train.csv"), index=False)
+    X_test.to_csv(os.path.join(output_path,"X_test.csv"), index=False)
+    y_train.to_csv(os.path.join(output_path,"y_train.csv"), index=False)
+    y_test.to_csv(os.path.join(output_path,"y_test.csv"), index=False)
 
 if __name__ == '__main__':
     main()    
