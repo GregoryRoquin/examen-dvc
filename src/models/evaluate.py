cc2f07d4475a19e048dd85d1b3d981a20cc349ff
GregoryRoquin <roquin.gregory@gmail.com>
2025-11-05 17:37:02 +0000

    ajout evaluate

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -3,13 +3,21 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from joblib import load
 import json
+import click
+import os
 
-def main():
+@click.command()
+@click.argument('x_test_scaled_filepath', type=click.Path(exists=False), required=True)
+@click.argument('y_test_filepath', type=click.Path(exists=False), required=True)
+@click.argument('models_filepath', type=click.Path(exists=False), required=True)
+@click.argument('metrics_path', type=click.Path(exists=False), required=True)
+@click.argument('prediction_path', type=click.Path(exists=False), required=True)
+def main(x_test_scaled_filepath, y_test_filepath, models_filepath, metrics_path, prediction_path):
     try:
-        model = load("models/gbr_model.pkl")
+        model = load(models_filepath)
 
-        X_test_scaled = pd.read_csv("data/processed_data/X_test_scaled.csv", header=0)
-        y_test = pd.read_csv("data/processed_data/y_test.csv", header=0).values.ravel()
+        X_test_scaled = pd.read_csv(x_test_scaled_filepath, header=0)
+        y_test = pd.read_csv(y_test_filepath, header=0).values.ravel()
 
         print("Prédiction des valeurs de test...")
         y_pred = model.predict(X_test_scaled)
@@ -23,7 +31,7 @@ def main():
         }
         print(f"Métriques calculées : {metrics}")
 
-        with open("metrics/scores.json", "w") as f:
+        with open(os.path.join(metrics_path,"scores.json"), "w") as f:
             json.dump(metrics, f, indent=4)
         print("Métriques sauvegardées dans 'metrics/scores.json' !")
 
@@ -32,8 +40,8 @@ def main():
         X_test_with_predictions["y_true"] = y_test
         X_test_with_predictions["y_pred"] = y_pred
 
-        X_test_with_predictions.to_csv("data/prediction.csv", index=False)
-        print("Dataset avec prédictions sauvegardé dans 'data/prediction.csv' !")
+        X_test_with_predictions.to_csv(os.path.join(prediction_path,"prediction.csv"), index=False)
+        print("Dataset avec prédictions sauvegardé !")
 
     except FileNotFoundError as e:
         print(f"Erreur : Fichier introuvable - {e}")
