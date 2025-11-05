c27d14b2990ce7bdc2aac6ea9fa960973f95bd49
GregoryRoquin <roquin.gregory@gmail.com>
2025-11-05 17:21:54 +0000

    ajout training

--- src/models/training.py
+++ src/models/training.py
@@ -1,19 +1,26 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from joblib import load, dump
+import click
+import os
 
-def main():
+@click.command()
+@click.argument('x_train_scaled_filepath', type=click.Path(exists=False), required=True)
+@click.argument('y_train_filepath', type=click.Path(exists=False), required=True)
+@click.argument('best_params_filepath', type=click.Path(exists=False), required=True)
+@click.argument('models_path', type=click.Path(exists=False), required=True)
+def main(x_train_scaled_filepath, y_train_filepath, best_params_filepath, models_path):
     try:
-        X_train_scaled = pd.read_csv("data/processed_data/X_train_scaled.csv", header=0)
-        y_train = pd.read_csv("data/processed_data/y_train.csv", header=0).values.ravel()
+        X_train_scaled = pd.read_csv(x_train_scaled_filepath, header=0)
+        y_train = pd.read_csv(y_train_filepath, header=0).values.ravel()
 
-        best_params = load("models/best_params.pkl")
+        best_params = load(best_params_filepath)
 
         model = RandomForestRegressor(**best_params, random_state=42)
 
         model.fit(X_train_scaled, y_train)
 
-        dump(model, "models/gbr_model.pkl")
+        dump(model, os.path.join(models_path,"gbr_model.pkl"))
 
         print("Modèle sauvegardé avec succès !")
 
